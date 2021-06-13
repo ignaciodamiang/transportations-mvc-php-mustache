d5bca14400636e32c145fb9ce1a15330336b1b7d
Ignacio Damián González <ignacigonzalez@alumno.unlam.edu.ar>
2021-06-13 15:27:12 -0300

    Maqueta home y login

---
+++ login.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.83.1">
+    <title>Signin</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/sign-in/">
+
+    <!-- Bootstrap core CSS -->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+
+    
+    <!-- Custom styles for this template -->
+    <link href="css/signin.css" rel="stylesheet">
+  </head>
+  <body class="bg-light text-center">
+    
+<main class="form-signin">
+  <form>
+    <img class="mb-4" src="img/imagotipo.svg" alt="" width="250" height="250">
+    <div class="form-floating">
+      <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+      <label for="floatingInput">Email</label>
+    </div>
+    <div class="form-floating">
+      <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+      <label for="floatingPassword">Password</label>
+    </div>
+
+    <div class="checkbox mb-3">
+      <label>
+        <input type="checkbox" value="remember-me"> Recordarme
+      </label>
+    </div>
+    <button class="w-100 btn btn-lg btn-primary" type="submit">Iniciar sesión</button>
+    <button class="mt-2 w-80 btn btn-lg btn-success" type="submit">Crear nueva cuenta</button>
+    <p class="mt-5 mb-3 text-muted">&copy;2021</p>
+  </form>
+</main>
+
+
+    
+  </body>
+</html>
