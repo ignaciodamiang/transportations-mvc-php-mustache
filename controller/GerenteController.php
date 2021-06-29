9ae72f7f94349ded360d6313959dc290bc4f5f02
ivankek <ivan.kek@hotmail.com>
2021-06-29 13:55:39 -0300

    Gerente controller

--- controller/GerenteController.php
+++ controller/GerenteController.php
@@ -23,6 +23,33 @@ class GerenteController
         }
     }
 
+    public function registrarViaje()
+    {
+
+        $CiudadOrigen = $_POST["ciudad_origen"];
+        $CiudadDestino = $_POST["ciudad_destino"];
+        $FechaInicio = $_POST["fecha_inicio"];
+        $HoraInicio = $_POST["hora_inicio"];
+        $FechaFin = $_POST["fecha_fin"];
+        $HoraFin = $_POST["hora_fin"];
+        $TiempoEstimado = $_POST["tiempo_estimado"];
+        $TiempoReal = $_POST["tiempo_real"];
+        $TipoCarga = $_POST["tipo_carga"];
+        $KmPrevisto = $_POST["km_previsto"];
+        $KmReales = $_POST["km_reales"];
+        $Desviacion = $_POST["desviacion"];
+        $PosicionGps = $_POST["posicion_gps"];
+        $CombustibleEstimado = $_POST["combustible_estimado"];
+        $CombustibleReal = $_POST["combustible_real"];
+        $Vehiculo = $_POST["id_vehiculo"];
+        $Chofer = $_POST["id_usuario"];
+
+        $this->GerenteModel->registrarViaje($CiudadOrigen, $CiudadDestino, $FechaInicio, $HoraInicio, $FechaFin, $HoraFin, $TiempoEstimado, $TiempoReal, $TipoCarga, $KmPrevisto, $KmReales, $Desviacion, $PosicionGps, $CombustibleEstimado, $CombustibleReal, $Vehiculo, $Chofer);
+
+        header("location: ../gerente");
+
+    }
+
     public function validarSesion()
     {
         $sesion = $_SESSION["Usuario"];
--- model/GerenteModel.php
+++ model/GerenteModel.php
@@ -9,5 +9,14 @@ class GerenteModel
         $this->database = $database;
     }
 
+    public function registrarViaje($ciudad_origen, $ciudad_destino, $fecha_inicio, $hora_inicio, $fecha_fin, $hora_fin, $tiempo_estimado, $tiempo_real, $tipo_carga, $km_previsto, $km_reales, $desviacion, $posicion_gps, $combustible_estimado, $combustible_real, $id_vehiculo, $id_usuario)
+    {
+        $sql = "INSERT INTO Viaje (ciudad_origen, ciudad_destino, fecha_inicio, hora_inicio, fecha_fin, hora_fin, tiempo_estimado, tiempo_real, tipo_carga, km_previsto, km_reales, desviacion, posicion_gps, combustible_estimado, combustible_real, id_vehiculo, id_usuario)
+VALUES( '$ciudad_origen', '$ciudad_destino', '$fecha_inicio', '$hora_inicio', '$fecha_fin', '$hora_fin', '$tiempo_estimado', '$tiempo_real', '$tipo_carga', '$km_previsto', '$km_reales', '$desviacion', '$posicion_gps', '$combustible_estimado', '$combustible_real', '$id_vehiculo', '$id_usuario')";
+
+        $this->database->query($sql);
+
+
+    }
 
 }
