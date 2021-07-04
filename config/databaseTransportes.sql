b0abdaf7c0a6ee6a9da594a841ee0ebacd07b6a3
ivankek <ivan.kek@hotmail.com>
2021-07-04 18:41:12 -0300

    cambios en tablas

--- config/databaseTransportes.sql
+++ config/databaseTransportes.sql
@@ -23,6 +23,12 @@ primary key (id),
 foreign key(id_tipoUsuario) references TipoUsuario(id)
 );
 
+create table TipoVehiculo(
+id int auto_increment,
+tipo_vehiculo varchar(50),
+primary key(id)
+);
+
 create table Arrastre(
 id int auto_increment,
 patente varchar(40),
@@ -47,7 +53,8 @@ kilometraje int,
 estado varchar(50),
 alarma varchar(50),
 id_tipoVehiculo int,
-primary key (id)
+primary key (id),
+foreign key (id_tipoVehiculo) references TipoVehiculo(id)
 );
 
 create table services(
@@ -69,71 +76,58 @@ create table cliente(
     apellido varchar(50), 
     primary key(id)
 );
-create table factura(
+
+create table Viaje(
+id int auto_increment,
+ciudad_origen varchar(50),
+ciudad_destino varchar(50),
+fecha_inicio date,
+fecha_inicioReal date,
+hora_inicio time,
+fecha_fin date,
+fecha_finReal date,
+hora_fin time,
+tiempo_estimado varchar(50),
+tiempo_real varchar(50),
+km_previsto double,
+km_reales double,
+desviacion double,
+combustible_estimado double,
+precioCombustible_estimado double,
+precioViaticos_estimado double,
+precioPeajes_estimado double,
+precioExtras_estimado double,
+precioFee_estimado double,
+precioHazard_estimado double,
+precioReefer_estimado double,
+precioTotal_estimado double,
+precioExtras_real double,
+precioViaticos_Real double,
+precioPeajes_Real double, 
+combustible_real double,
+precioCombustible_real double,
+precioTotal_real double,
+viaje_enCurso boolean,
+id_arrastre int,
+id_vehiculo int,
+id_usuario int,
+primary key(id),
+foreign key(id_vehiculo) references Vehiculo(id),
+foreign key(id_arrastre) references Arrastre(id),
+foreign key(id_usuario) references Usuario(id)
+);
+
+create table Factura(
     id int auto_increment,
     fecha varchar(50),
     monto double,
     abonado boolean, 
     descripcion varchar(100),
     id_cliente int,
+    id_viaje int,
     primary key(id),
+    foreign key(id_viaje) references Viaje(id),
     foreign key(id_cliente) references Cliente(id)
-);
-
-create table Viaje(
-id int auto_increment,
-ciudad_origen varchar(50),
-ciudad_destino varchar(50),
-fecha_inicioReal date,
-fecha_finReal date,
-hora_inicio time,
-hora_fin time,
-tiempo_real varchar(50),
-km_reales double,
-desviacion double,
-precioViaticos_Real double,
-peajes_Real double, 
-combustible_real double,
-precioCombustible_real double,
-precioExtras_real int,
-precioTotal_real double,
-id_cliente int,
-id_vehiculo int,
-id_arrastre int,
-id_usuario int,
-primary key(id),
-foreign key(id_cliente) references Cliente(id),
-foreign key(id_vehiculo) references Vehiculo(id),
-foreign key(id_arrastre) references Arrastre(id),
-foreign key(id_usuario) references Usuario(id)
-);
-
-create table Proforma(
-id int auto_increment,
-fecha_inicioEstimada date,
-fecha_finEstimada date,
-tiempo_estimado varchar(50),
-km_previsto double,
-combustible_previsto double,
-precioViaticos_estimado double,
-precioPeajes_estimado double,
-precioCombustible_estimado double,
-precioExtras_estimado double,
-precioFee_estimado double,
-precioHazard_estimado double,
-precioReefer_estimado double,
-precioTotal_estimado double,
-id_cliente int,
-id_vehiculo int,
-id_arrastre int,
-id_usuario int,
-id_viaje int,
-primary key(id),
-foreign key(id_cliente) references Cliente(id),
-foreign key(id_vehiculo) references Vehiculo(id),
-foreign key(id_arrastre) references Arrastre(id),
-foreign key(id_usuario) references Usuario(id),
-foreign key(id_viaje) references Viaje(id)
 );
 
 create table ProformaChofer(
@@ -182,6 +176,8 @@ values(9, "tomas", "lala", 2, 89878625, "05-02-1989","to@gmail.com", "1234", tru
 insert into Usuario(id, nombre, apellido, legajo, dni, fecha_nacimiento, tipo_licencia ,id_tipoUsuario, email, contraseña, cuenta_activada)
 values(5, "cho2", "chofer2", 4, 44, "01-01-1999","A" , 3, "chofer2@gmail.com", "1234", true);
  
-select * from Viaje;
+/*select * from Viaje;
 select * from Usuario WHERE id_tipoUsuario = '3';
 select * from Usuario;
+
+select * from Viaje WHERE id_usuario = '5' and viaje_enCurso = 1;*/
