3259d5ede6c2b39fd99ff293ab13da85de44c120
ivankek <ivan.kek@hotmail.com>
2021-07-04 17:08:13 -0300

    Cambios en las tablas

--- config/databaseTransportes.sql
+++ config/databaseTransportes.sql
@@ -23,9 +23,15 @@ primary key (id),
 foreign key(id_tipoUsuario) references TipoUsuario(id)
 );
 
-create table TipoVehiculo(
+create table Arrastre(
 id int auto_increment,
-tipo_vehiculo varchar(50),
+patente varchar(40),
+numeroDeChasis int,
+tipo varchar(50),
+peso_Neto float not null,
+hazard boolean not null,
+reefer boolean not null,
+temperatura DECIMAL(5,2),
 primary key(id)
 );
 
@@ -41,8 +47,7 @@ kilometraje int,
 estado varchar(50),
 alarma varchar(50),
 id_tipoVehiculo int,
-primary key (id),
-foreign key (id_tipoVehiculo) references TipoVehiculo(id)
+primary key (id)
 );
 
 create table services(
@@ -79,27 +84,73 @@ create table Viaje(
 id int auto_increment,
 ciudad_origen varchar(50),
 ciudad_destino varchar(50),
-fecha_inicio date,
 fecha_inicioReal date,
-hora_inicio time,
-fecha_fin date,
 fecha_finReal date,
+hora_inicio time,
 hora_fin time,
-tiempo_estimado varchar(50),
 tiempo_real varchar(50),
-tipo_carga varchar(50),
-km_previsto double,
 km_reales double,
 desviacion double,
-posicion_gps varchar(50),
-combustible_estimado double, 
+precioViaticos_Real double,
+peajes_Real double, 
 combustible_real double,
-viaje_enCurso boolean,
+precioCombustible_real double,
+precioExtras_real int,
+precioTotal_real double,
+id_cliente int,
 id_vehiculo int,
+id_arrastre int,
 id_usuario int,
 primary key(id),
+foreign key(id_cliente) references Cliente(id),
 foreign key(id_vehiculo) references Vehiculo(id),
+foreign key(id_arrastre) references Arrastre(id),
 foreign key(id_usuario) references Usuario(id)
+);
+
+create table Proforma(
+id int auto_increment,
+fecha_inicioEstimada date,
+fecha_finEstimada date,
+tiempo_estimado varchar(50),
+km_previsto double,
+combustible_previsto double,
+precioViaticos_estimado double,
+precioPeajes_estimado double,
+precioCombustible_estimado double,
+precioExtras_estimado double,
+precioFee_estimado double,
+precioHazard_estimado double,
+precioReefer_estimado double,
+precioTotal_estimado double,
+id_cliente int,
+id_vehiculo int,
+id_arrastre int,
+id_usuario int,
+id_viaje int,
+primary key(id),
+foreign key(id_cliente) references Cliente(id),
+foreign key(id_vehiculo) references Vehiculo(id),
+foreign key(id_arrastre) references Arrastre(id),
+foreign key(id_usuario) references Usuario(id),
+foreign key(id_viaje) references Viaje(id)
+);
+
+create table ProformaChofer(
+id_notificacion int not null auto_increment,
+fecha_Inicio date,
+latitud_actual varchar(50),
+longitud_actual varchar(50),
+combustible_actual double,
+precioCombustible_actual double,
+km_actuales double,
+desviacion_actual double,
+precioViaticos_actual double,
+precioPeajes_actual double,
+precioExtras_actual double,
+id_viaje int,
+primary key(id_notificacion),
+foreign key(id_viaje) references Viaje(id)
 );
 
 insert into TipoUsuario(id, rol)
@@ -117,11 +168,6 @@ values(3, "chofer", "chofer", 2, 22333444, "01-01-1999","A" , 3, "chofer@gmail.c
 insert into Usuario(id, nombre, apellido, legajo, dni, fecha_nacimiento, tipo_licencia ,id_tipoUsuario, email, contraseña, cuenta_activada)
 values(4, "mecanico", "mecanico", 2, 33444555, "01-01-1999","A" , 4, "mecanico@gmail.com", "1234", true);
 
-insert into TipoVehiculo(id , tipo_vehiculo)
-values (1,"camioneta"),
-		(2,"camion"),
-        (3,"moto"),
-        (4,"auto");
 /* usuarios sin rol*/
 
 insert into Usuario(id, nombre, apellido, legajo, dni, fecha_nacimiento, email, contraseña, cuenta_activada)
@@ -139,5 +185,3 @@ values(5, "cho2", "chofer2", 4, 44, "01-01-1999","A" , 3, "chofer2@gmail.com", "
 select * from Viaje;
 select * from Usuario WHERE id_tipoUsuario = '3';
 select * from Usuario;
-
-select * from Viaje WHERE id_usuario = '5' and viaje_enCurso = 1;
