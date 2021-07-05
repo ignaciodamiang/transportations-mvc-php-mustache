01f5224e91eeb144876aaa82f171e606243e0c1a
E3MiLiAn0 <ovejero.emiliano@hotmail.es>
2021-07-05 16:21:35 -0300

    formulario chofer

--- model/ChoferModel.php
+++ model/ChoferModel.php
@@ -12,9 +12,19 @@ class ChoferModel
 
     public function getViajeEnCurso($idChofer){
         $sql = "SELECT * FROM Viaje INNER JOIN TipoVehiculo ON Viaje.id_vehiculo = TipoVehiculo.id
-                WHERE id_usuario = '3' and viaje_enCurso = 1";
+                WHERE id_usuario = '3' and viaje_enCurso =0";
         $consulta = $this->database->query($sql);
         return $consulta;
+    }
+
+    public function getEmpezarViaje($id_viaje,$latitud, $longitud,$fechaInicioReal,$horaInicioReal){
+        $sql="UPDATE `transporteslamatanza`.`Viaje` 
+                SET `latitud_inicio` = '$latitud', `longitud_inicio` = '$longitud' ,
+                fecha_inicioReal='$fechaInicioReal', hora_inicio='$horaInicioReal',
+                viaje_enCurso='true';
+                WHERE (`id` = '$id_viaje')";
+        $this->database->execute($sql);
+
     }
 
 
